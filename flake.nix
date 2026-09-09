higherorderfunctor/nix-agentic-tools

feat(strictdoc): a commentary remarks on the canon itself
The operator: "i dont like general NARRATIVE nodes used to document things
like gaps or other arch notes. i think this should be a new class of core
cannon grammar for effectively commentary nodes I can run semantic
validators against."

The meta DSL that replaces is concrete: a narrative with WIDGET: rows
carries its verdicts as a bracket at the end of a line, parsed by a regex.
A validator cannot ask that structure whether a gap names what would close
it, because the whole row is one string. COMMENTARY makes CLOSES_ON a
required field, so the grammar enforces it.

EDGE is the part with no precedent. A strictdoc relation is a
TYPE/VALUE/ROLE triple inside a node with no identity of its own, so there
is nothing to point a UID at; an edge is named structurally as
"<FROM> <ROLE> <TO>", and both endpoints are ALSO written as Remarks_On
relations. That redundancy is the feature: a Remarks_On to a UID that does
not exist fails the export, so writing the ends twice buys parser-enforced
existence for a field the parser cannot read.

THE ELEMENT SHIPS EMPTY, and its gate ships with it because
INV-GUARDS-MUST-EXECUTE forbids the other order. Nothing in the corpus is a
COMMENTARY. DEC-COMMENTARY-IS-THE-FIFTH-FAMILY is filed OPEN: whether this
is a fifth family or the odd member of Representation turns on one sentence
of DEC-NODE-FAMILIES, sixteen narratives cite that decision, and no code
reads the family -- so the ruling costs nothing to defer and something to
guess.

Nothing is retyped. Every ordering for converting the five existing gap
narratives was measured and each loses a gap SILENTLY: retype-and-keep-
Contains fires a finding and the walk continues, so the gap vanishes from
the tab on a green build. The Gaps tab is the one screen already meeting
the aspiration; breaking it to demonstrate a mechanism is the wrong trade.
The row bracket stays legal and DEC-ROW-REFERENCE-LINES stays in force --
494 rows draw thro

flake.nix
@@ -284,6 +284,7 @@
       shellcheckCorpusCheck = {shellcheck-corpus = import ./checks/shellcheck-corpus.nix {inherit lib pkgs;};};
       splitCodeSpansCheck = {split-code-spans = import ./checks/split-code-spans.nix {inherit pkgs;};};
       strictdocCycleCheck = {strictdoc-cycle-check = import ./checks/strictdoc-cycle-check.nix {inherit pkgs self;};};
+      strictdocCommentaryCheck = {strictdoc-commentary-check = import ./checks/strictdoc-commentary-check.nix {inherit pkgs self;};};
       # The grammar extractor's environment, built ONCE for the four checks
       # that run it. It is not an overlay attribute: an overlay converts an
       # upstream package, wrapping belongs to the consumer, and the consumer
@@ -305,7 +306,7 @@
       updateTargetsParityCheck = {update-targets-parity = import ./checks/update-targets-parity.nix {inherit inputs lib pkgs self updateRegistry;};};
       validateAtStopCheck = {validate-at-stop = import ./checks/validate-at-stop.nix {inherit pkgs;};};
     in
-      bareCommandsCheck // beadsContractsCheck // beadsLifecycleCheck // cacheHitParityCheck // claudeDelegationClampCheck // claudeDevenvHooksRealTypeCheck // claudeExtractedCheck // claudeHeronBrookCheck // claudeMemoryCollisionGuardCheck // claudeSettingsSchemaCheck // codexCoverageCheck // codexExtractedCheck // copilotWrapperArgvCheck // doubledWordsCheck // doubledWordsFixturesCheck // facetMockChecks // factoryChecks // formattingCheck // fragmentsChecks // glabExtractedCheck // goFloorDriftChecks // goToolchainFloorChecks // instructionsDriftCheck // kiroExtractedCheck // kiroFhsContractCheck // kiroWrapperArgvCheck // moduleChecks // optionsDocsCheck // pnpmFetcherParityCheck // sembleTemplatesCheck // shellcheckCorpusCheck // splitCodeSpansCheck // strictdocCycleCheck // strictdocFileCheck // strictdocFpCheck // strictdocGrammarCorpusCheck // strictdocGrammarForeignRoundtripCheck // strictdocGrammarModelEqualCheck // strictdocGrammarNegativeFixturesCheck // strictdocGrammarSurfaceCurrentCheck // strictdocGrammarSurfaceLiveCheck // updateTargetsParityCheck // validateAtStopCheck);
+      bareCommandsCheck // beadsContractsCheck // beadsLifecycleCheck // cacheHitParityCheck // claudeDelegationClampCheck // claudeDevenvHooksRealTypeCheck // claudeExtractedCheck // claudeHeronBrookCheck // claudeMemoryCollisionGuardCheck // claudeSettingsSchemaCheck // codexCoverageCheck // codexExtractedCheck // copilotWrapperArgvCheck // doubledWordsCheck // doubledWordsFixturesCheck // facetMockChecks // factoryChecks // formattingCheck // fragmentsChecks // glabExtractedCheck // goFloorDriftChecks // goToolchainFloorChecks // instructionsDriftCheck // kiroExtractedCheck // kiroFhsContractCheck // kiroWrapperArgvCheck // moduleChecks // optionsDocsCheck // pnpmFetcherParityCheck // sembleTemplatesCheck // shellcheckCorpusCheck // splitCodeSpansCheck // strictdocCommentaryCheck // strictdocCycleCheck // strictdocFileCheck // strictdocFpCheck // strictdocGrammarCorpusCheck // strictdocGrammarForeignRoundtripCheck // strictdocGrammarModelEqualCheck // strictdocGrammarNegativeFixturesCheck // strictdocGrammarSurfaceCurrentCheck // strictdocGrammarSurfaceLiveCheck // updateTargetsParityCheck // validateAtStopCheck);
 
     # devShells.default provided by devenv CLI (devenv shell / devenv test)
     # from devenv.nix; nothing in this flake constructs it.

higherorderfunctor/agentic-tools

flake.nix
@@ -313,6 +313,7 @@
       };
       splitCodeSpansCheck = {split-code-spans = import ./checks/split-code-spans.nix {inherit pkgs;};};
       strictdocCycleCheck = {strictdoc-cycle-check = import ./checks/strictdoc-cycle-check.nix {inherit pkgs self;};};
+      strictdocCommentaryCheck = {strictdoc-commentary-check = import ./checks/strictdoc-commentary-check.nix {inherit pkgs self;};};
       # The grammar extractor's environment, built ONCE for the four checks
       # that run it. It is not an overlay attribute: an overlay converts an
       # upstream package, wrapping belongs to the consumer, and the consumer
@@ -335,7 +336,7 @@
       prWatchAtStopCheck = {pr-watch-at-stop = import ./checks/pr-watch-at-stop.nix {inherit pkgs;};};
       validateAtStopCheck = {validate-at-stop = import ./checks/validate-at-stop.nix {inherit pkgs;};};
     in
-      bareCommandsCheck // beadsContractsCheck // beadsLifecycleCheck // cacheHitParityCheck // claudeDelegationClampCheck // claudeDevenvHooksRealTypeCheck // claudeExtractedCheck // claudeHeronBrookCheck // claudeMemoryCollisionGuardCheck // claudeSettingsSchemaCheck // codexCoverageCheck // codexExtractedCheck // copilotWrapperArgvCheck // doubledWordsCheck // doubledWordsFixturesCheck // markdownTableCellsFixturesCheck // facetMockChecks // factoryChecks // formattingCheck // fragmentsChecks // glabExtractedCheck // goFloorDriftChecks // goFloorExtractOrderChecks // goToolchainFloorChecks // instructionMaterializationCheck // instructionsDriftCheck // isolatePrekHooksCheck // kiroExtractedCheck // kiroFhsContractCheck // kiroWorkspaceSettingsFixturesCheck // kiroWrapperArgvCheck // moduleChecks // optionsDocsCheck // pnpmFetcherContractCheck // pnpmFetcherParityCheck // repoValidationChecks // sembleTemplatesCheck // splitCodeSpansCheck // strictdocCycleCheck // strictdocFileCheck // strictdocFpCheck // strictdocGrammarCorpusCheck // strictdocGrammarForeignRoundtripCheck // strictdocGrammarModelEqualCheck // strictdocGrammarNegativeFixturesCheck // strictdocGrammarSurfaceCurrentCheck // strictdocGrammarSurfaceLiveCheck // updateTargetsParityCheck // prWatchAtStopCheck // validateAtStopCheck);
+      bareCommandsCheck // beadsContractsCheck // beadsLifecycleCheck // cacheHitParityCheck // claudeDelegationClampCheck // claudeDevenvHooksRealTypeCheck // claudeExtractedCheck // claudeHeronBrookCheck // claudeMemoryCollisionGuardCheck // claudeSettingsSchemaCheck // codexCoverageCheck // codexExtractedCheck // copilotWrapperArgvCheck // doubledWordsCheck // doubledWordsFixturesCheck // markdownTableCellsFixturesCheck // facetMockChecks // factoryChecks // formattingCheck // fragmentsChecks // glabExtractedCheck // goFloorDriftChecks // goFloorExtractOrderChecks // goToolchainFloorChecks // instructionMaterializationCheck // instructionsDriftCheck // isolatePrekHooksCheck // kiroExtractedCheck // kiroFhsContractCheck // kiroWorkspaceSettingsFixturesCheck // kiroWrapperArgvCheck // moduleChecks // optionsDocsCheck // pnpmFetcherContractCheck // pnpmFetcherParityCheck // repoValidationChecks // sembleTemplatesCheck // splitCodeSpansCheck // strictdocCommentaryCheck // strictdocCycleCheck // strictdocFileCheck // strictdocFpCheck // strictdocGrammarCorpusCheck // strictdocGrammarForeignRoundtripCheck // strictdocGrammarModelEqualCheck // strictdocGrammarNegativeFixturesCheck // strictdocGrammarSurfaceCurrentCheck // strictdocGrammarSurfaceLiveCheck // updateTargetsParityCheck // prWatchAtStopCheck // validateAtStopCheck);
 
     # devShells.default provided by devenv CLI (devenv shell / devenv test)
     # from devenv.nix; nothing in this flake constructs it.